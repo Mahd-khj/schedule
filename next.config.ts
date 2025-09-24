9689200cd5c3ba154c8f3b97263424a7e26fd13a
Mahd-khj <mahdi.khj98@gmail.com>
2025-09-24 22:01:28 +0300

    problome ovride

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ✅ Allow production builds to succeed even if ESLint errors are present
+    ignoreDuringBuilds: true,
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
